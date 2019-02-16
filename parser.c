c06a77c0dccc16dbf42e858d56dc97d1dd1af260
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-02-15 16:55:50 -0800

    getexp

--- parser.c
+++ parser.c
@@ -1452,7 +1452,8 @@ AST* BinaryExpr(TokenList** tokens,
                          (*tokens)->t->linenum);
       AppendAST(ast, first_ast);
       for (int i = 0; i < size; i++) {
-          if (ProcessToken((*tokens)->next, possible_tokens[i])) {
+          (*tokens)->next;
+          if (ProcessToken(tokens, possible_tokens[i])) {
               AppendAST(ast, BinaryExpr(tokens, possible_tokens, ast_choices, size, type));
               return ast;
           }
