0c77d1f24fb3a1219d1b8e5bfdd3c32071e5ddde
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-02-15 16:54:43 -0800

    getexp

--- parser.c
+++ parser.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include "utils.h"
 #include "ast.h"
@@ -1453,8 +1452,7 @@ AST* BinaryExpr(TokenList** tokens,
                          (*tokens)->t->linenum);
       AppendAST(ast, first_ast);
       for (int i = 0; i < size; i++) {
-          if (ProcessToken(tokens, possible_tokens[i])) {
-              printf("hi");
+          if (ProcessToken((*tokens)->next, possible_tokens[i])) {
               AppendAST(ast, BinaryExpr(tokens, possible_tokens, ast_choices, size, type));
               return ast;
           }
